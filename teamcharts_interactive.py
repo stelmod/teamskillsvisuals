ea0539e87da0219637bb0d454f9076e905ac3c83
Stelios Modes <smodes@thoughtworks.com>
2017-07-16 14:20:47 +0200

    Various useful imports

--- teamcharts_interactive.py
+++ teamcharts_interactive.py
@@ -3,5 +3,13 @@
 
 # In[ ]:
 
-
+get_ipython().magic('matplotlib inline')
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.path import Path
+from matplotlib.spines import Spine
+from matplotlib.projections.polar import PolarAxes
+from matplotlib.projections import register_projection
+import seaborn as sns
 
